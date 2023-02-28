5401f5d91889be7f42651d3a008d384fa6efec38
muhammadfagi <83048520+muhammadfagi@users.noreply.github.com>
2023-02-28 16:15:01 +0700

    coba edit

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_counter tes',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
